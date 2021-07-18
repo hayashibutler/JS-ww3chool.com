0436a974eb60e8456787e00aa8169e213c60da7f
Tran Lam <hayashibutler@gmail.com>
2021-07-18 16:32:38 +0700

    first

---
+++ java.js
@@ -0,0 +1,8 @@
+function myFunction() {
+		document.getElementById('where2').innerHTML="van ban da thay doi";}
+function toCelsius(f) {
+	return 5/9*(f-32);
+};
+function displayDate() {
+	document.getElementById('events1').innerHTML=Date()
+}
